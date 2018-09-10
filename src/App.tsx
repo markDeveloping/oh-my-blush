1b28d8c00adec0e15929f1d960dbba2b50c41150
markDeveloping <mark.developing@gmail.com>
2018-09-10 21:16:19 +0100

    made desktop stylesheet.

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,11 @@ class App extends React.Component {
         <div className="product--heading">
           <h2>Products</h2>
         </div>
+        <div className="product-grid--container">
         <Product name="Ellie" metaDescription="Pretty floral letters filled with high quality artificial roses, peonies and greenery" cost={150} img={love} imgAlt="Picture of our love letters at a wedding" colour="pink" />
         <Product name="Maddie" metaDescription="A beautiful blend of pale pink, blush and nude flowers - 2.5 X 2.5m" cost={200} img={pinkwall} imgAlt="Picture of our pale pink and nutural flower wall" colour="blue" />
         <Product name="Gracie" metaDescription="A perfect contrast between rich foliage and classic white flowers - 2.5 x 2.5m" cost={200} img={whitewall} imgAlt="Picture of our white flower wall" colour="pink" />        
+        </div>
         <Footer email="ohmyblush.eventhire@hotmail.com" contactLabel="Contact us" telephone="0749 6539 219" street="Tintern Close" town="Bristol" postcode="BS30 7XH"/>
       </div>
     );
